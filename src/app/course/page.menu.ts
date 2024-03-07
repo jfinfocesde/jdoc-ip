0d1823767806b7b1aef937f972a161817df2baba
jfinfocesde <110859321+jfinfocesde@users.noreply.github.com>
2024-03-07 07:24:47 -0500

    7-3-2024

---
+++ src/app/course/page.menu.ts
@@ -0,0 +1,92 @@
+interface subItemsInfoType {
+    href: string,
+    label: string,
+    date: string,
+}
+
+interface itemsInfoType {
+    href: string,
+    label: string,
+    date: string,
+    submenu: subItemsInfoType[]
+}
+
+export const modedev = false
+
+export const infoCourse = {
+    title: "Introducción a la programación de Programación",
+    description: "En este curso, aprenderás los fundamentos del desarrollo web con HTML y CSS. Te enseñaremos cómo crear páginas web que sean atractivas, funcionales y fáciles de usar.",
+    image: "https://pro.guslyon.fr/static/html-css-67ea05e3879abd11f82b077273760d2f.png"
+}
+
+export const itemsInfo: itemsInfoType[] = [
+    {
+        href: "/course",
+        label: "Introducción",
+        date: '2024-2-15',
+        submenu: []
+    },
+    {
+        href: "/course/contents/semana1",
+        label: "Semana 1",
+        date: '2024-2-15',
+        submenu: [
+            {
+                href: "/course/contents/semana1",
+                label: "Pacto pedagógico",
+                date: '2024-2-15',
+            }            
+        ]
+    },
+    {
+        href: "",
+        label: "Semana 2",
+        date: '2024-2-22',
+        submenu: [
+            {
+                href: "/course/contents/semana2",
+                label: "Contenido",
+                date: '2024-2-22',
+            },
+            {
+                href: "/course/activities/actividad1",
+                label: "Actividad 1",
+                date: '2024-2-22',
+            },
+        ]
+    },
+    {
+        href: "",
+        label: "Semana 3",
+        date: '2024-2-29',
+        submenu: [
+            {
+                href: "/course/contents/semana3",
+                label: "Contenido",
+                date: '2024-2-29',
+            },
+            {
+                href: "/course/activities/actividad2",
+                label: "Actividad 2",
+                date: '2024-2-29',
+            },
+        ]
+    },
+    {
+        href: "",
+        label: "Semana 4",
+        date: '2024-3-7',
+        submenu: [
+            {
+                href: "/course/contents/semana4",
+                label: "Contenido",
+                date: '2024-3-7',
+            },
+            {
+                href: "/course/activities/actividad3",
+                label: "Actividad 3",
+                date: '2024-3-7',
+            },
+        ]
+    },    
+]
